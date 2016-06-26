2ac26598e0eca625873a888af36734ab7a97b249
KennyYahir <kenny.puma1030@gmail.com>
2016-06-26 02:46:14 -0500

    Se termina examen

--- Mineria/Ejercicio-2-Examen.R
+++ Mineria/Ejercicio-2-Examen.R
@@ -8,15 +8,17 @@ data_bin
 summary(data_bin)
 ftable(data_bin, row.vars=NULL, col.vars="Play")
 
-data_tree<-rpart(Play ~ ., data_bin, control=rpart.control(minsplit=1), parms = list(split = "gini"))
-data_tree
+modelo<-rpart(Play ~ ., data_bin, control = rpart.control(minsplit = 1), 
+			  parms = list(split = "gini"))
+#modelo
 
-rpart.plot(data_tree)
+rpart.plot(modelo)
 
-data_pred <-read.csv("Datos_pred.csv", sep=",", header=T)
-str(data_pred)
-data_pred$Wind<-as.factor(data_pred$Wind)
+data_pred <-read.csv("tupla_pred.csv", sep=",", header=T)
+data_pred
 
-predict(data_tree, data_pred)
-p <- predict(data_tree, data_pred, type="class")
+data_pred$Wind <- factor(data_pred$Wind, levels = unique(data_bin$Wind))
+
+#predict(modelo, data_pred)
+p <- predict(modelo, data_pred, type="class")
 p
--- Mineria/Ejercicio-3-Examen.R
+++ Mineria/Ejercicio-3-Examen.R
@@ -1,19 +1,44 @@
 install.packages("neuralnet")
 library(neuralnet)
 
+sustVal <- function(x, ...){ # Recibimos x como un vector y '...' como pares
+	
+	lista <- list(...)
+	
+	for(elem in lista){
+		
+		oldVal <- elem[1]
+		newVal <- elem[2]
+		
+		pos <- x == oldVal
+		
+		x[pos] <- newVal
+	}
+	
+	x
+} # Devuelve un vector con valores sustituidos
+
 setwd("C:/Users/Atycus/Documents/ExamenDM")
+
 data <-read.csv("Datos2.csv", sep=",", header=T)
-data
+
 data<-data[1:8,]
-str(data)
-summary(data)
 
-data$a2<-(data$a2 - min(data$a2)) / (max(data$a2) - min(data$a2))
+data$a1 <- as.numeric(data$a1)
+data$a1 <- normaliza(data$a1)
+
+data$a2 <- normaliza(data$a2)
+data$a3 <- normaliza(data$a3)
+
+
+
 data
-set.seed(53)
-w<-matrix(runif(13,-.8,.8))
+set.seed(123)
+w <- matrix(runif(11,-.8,.8))
 
-net.data <- neuralnet(clase ~ a1_pos + a1_neg + a2 + a3, data[2,], hidden = c(2), algorithm = "backprop", learningrate = .9, startweights = w)
+net.data <- neuralnet(clase ~ a1 + a2 + a3, data[2,], 
+					  hidden = c(3), algorithm = "backprop", 
+					  learningrate = 0.7, startweights = w)
 
 net.data$call
 net.data$response
@@ -24,7 +49,8 @@ net.data$data
 net.data$net.result
 net.data$weights
 
-plot.nn(net.data, col.entry="blue", col.hidden.synapse = "red", col.intercept = "green")
+plot.nn(net.data, col.entry = "red", col.hidden.synapse = "orange", 
+		col.intercept = "black")
 
 #prediction(net.data)
 
