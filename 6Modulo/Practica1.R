39d219c61823057d1c778c987ed9f589d8b0998b
Fou-foo <fou-foo@hotmail.com>
2016-08-27 17:42:40 -0500

    Foo Kernel

--- 6Modulo/Practica1.R
+++ 6Modulo/Practica1.R
@@ -1,20 +1,38 @@
 
 dat1 <- read.csv("eleccciodeoptativas2016grupo2.xlsx - Base completa de eleccion.csv", 
 				 dec = ",")
-#dat2 <- read.csv("eleccciodeoptativas2016grupo2.xlsx - resumen.csv", header = F)[, 1:2]
-
-
+dat2 <- read.csv("eleccciodeoptativas2016grupo2.xlsx - resumen.csv", header = F)[, 1:2]
 
 library(reshape2)
 
 head(dat1)
-dat1
+head(dat2)
+colnames(dat2) <- c("materia", "clave")
 
+dat1 <- na.omit(dat1)
+dat2 <- na.omit(dat2)
 trainData.pre <- dat1[, c(1, 3:4)]
 
-trainData.melt <- melt(trainData.pre, id.vars = c("Individuo","Clave"))
-trainData <- acast(trainData.melt, Individuo ~ Clave)
+head(x<-melt(trainData.pre, id.vars = c("Individuo","Clave")))
+head(encoding <- acast(x,Individuo  ~ Clave))
+index <- is.na(encoding)
+encoding[index] <- 0
 
-head(trainData)
+head(encoding)
 
-trainData[is.na(trainData)] <- 0
+
+set.seed(0)
+varIn <- c()
+for (i in 1:30)
+{
+    varIn[i] <- tryCatch(sum(kmeans(encoding, centers = i)$withinss),
+                         warnings="warning", error="aqui")  #Por si algun modelo falla
+}
+
+##
+##
+
+
+
+
+
