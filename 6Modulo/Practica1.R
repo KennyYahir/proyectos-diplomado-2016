885a80ff27e1d4d57195d3869fd01fc5a57564ea
KennyYahir <kenny.puma1030@gmail.com>
2016-08-27 17:43:10 -0500

    Otra version mia medio compatible con el de toño

--- 6Modulo/Practica1.R
+++ 6Modulo/Practica1.R
@@ -1,20 +1,22 @@
 
 dat1 <- read.csv("eleccciodeoptativas2016grupo2.xlsx - Base completa de eleccion.csv", 
 				 dec = ",")
-#dat2 <- read.csv("eleccciodeoptativas2016grupo2.xlsx - resumen.csv", header = F)[, 1:2]
-
-
 
 library(reshape2)
 
+
+dat1 <- na.omit(dat1)
 head(dat1)
-dat1
 
 trainData.pre <- dat1[, c(1, 3:4)]
 
 trainData.melt <- melt(trainData.pre, id.vars = c("Individuo","Clave"))
 trainData <- acast(trainData.melt, Individuo ~ Clave)
 
+trainData[is.na(trainData)] <- 0
 head(trainData)
 
+
 trainData[is.na(trainData)] <- 0
+
+
