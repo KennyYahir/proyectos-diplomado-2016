6912b1b10578a57e2cb40322884977547a07e32d
KennyYahir <kenny.puma1030@gmail.com>
2016-09-13 21:02:36 -0500

    Fata pulir primera parte y dar conclusiones

--- 6Modulo/Practica3_1.R
+++ 6Modulo/Practica3_1.R
@@ -1,19 +1,22 @@
 
+library(psych)
+library(knitr)
+
 datos.brut <- read.csv("test_trabajo.csv", dec = ",")
 head(datos.brut)
 
 #install.packages("GPArotation") # GPArotation is required
-library(psych)
-library(knitr)
 
-datos.lim <- datos.brut#[-ncol(datos.brut)]
+datos.lim <- datos.brut[-c(1:3, 9)]
+head(datos.lim)
+#model.0 <- pca(datos.brut, 7)
+#model.0
 
-model.0 <- pca(datos.brut, 7)
-model.0
-
-model.1 <- fa(r = datos.lim, nfactors = 4, fm = "pa", rotate = "oblimin")
-class(model.1$loadings)
+model.1 <- fa(r = datos.lim, nfactors = 3, fm = "pa", rotate = "oblimin")
+model.1$loadings
 model.1
 
-as.data.frame(model.1$loadings)
-model.1$loadings
+model.1$communality[model.1$communality >= 0.7]
+model.1$communality[model.1$communality < 0.7]
+
+model.1$rotation
